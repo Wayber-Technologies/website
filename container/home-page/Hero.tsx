ee8ee198e125abafdc54afcdbc4e29798e684617
Vinyl-Davyl <okononfuadavid@gmail.com>
2025-09-15 09:35:38 +0100

    update: development

--- container/home-page/Hero.tsx
+++ container/home-page/Hero.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { ArrowUpRight } from "lucide-react";
-import { awwwards, backgroundAbout, ochiside } from "@/public";
-import { BackgroundImg, BackgroundVideo } from "@/components";
+import { awwwards } from "@/public";
+import { BackgroundVideo } from "@/components";
 
 export default function Hero() {
   return (
@@ -35,16 +35,58 @@ export default function Hero() {
                     }}
                     className="leading-[130px]"
                   >
-                    <Image
+                    {/* <Image
                       width={120}
                       height={50}
                       src={ochiside}
                       alt="img"
                       className="w-auto h-[95px] lg:w-auto lg:h-auto md:w-[100px] md:h-[63px] sm:w-[74px] sm:h-[45px] xm:w-[64px] xm:h-[40px] object-cover xl:mt-[15px] mt-[10px] rounded-[10px]"
+                    /> */}
+                    <video
+                      src="/heroVideo4.mp4"
+                      width={120}
+                      height={50}
+                      autoPlay
+                      loop
+                      muted
+                      playsInline
+                      className="hero-inline-video"
+                      style={{
+                        width: "auto",
+                        height: "95px",
+                        objectFit: "cover",
+                        marginTop: "10px",
+                        borderRadius: "10px",
+                      }}
                     />
+                    <style jsx>{`
+                      @media (min-width: 1491px) {
+                        .hero-inline-video {
+                          margin-top: 15px;
+                        }
+                      }
+                      @media (min-width: 769px) and (max-width: 1024px) {
+                        .hero-inline-video {
+                          width: 100px;
+                          height: 63px;
+                        }
+                      }
+                      @media (min-width: 401px) and (max-width: 768px) {
+                        .hero-inline-video {
+                          width: 74px;
+                          height: 45px;
+                        }
+                      }
+                      @media (max-width: 400px) {
+                        .hero-inline-video {
+                          width: 64px;
+                          height: 40px;
+                        }
+                      }
+                    `}</style>
                   </motion.span>
                   <h1 className="heading tracking-[-1.3px] text-black font-semibold font-FoundersGrotesk uppercase">
-                    build-broad
+                    build broad
                   </h1>
                 </div>
                 <motion.h1
@@ -56,7 +98,10 @@ export default function Hero() {
                   <span className="relative inline-block">
                     EXPERIENCE
                     <span className="absolute left-3/4 top-0 -translate-x-1/2 -translate-y-1/4 rotate-[13deg] bg-wayberLightBlue text-black text-xs sm:text-sm md:text-lg lg:text-xl px-2 py-1 sm:px-2 sm:py-1 md:px-2 md:py-1 rounded-md whitespace-nowrap font-bold tracking-normal font-aloevera">
-                      BUILDING TOMORROW&apos;S TECH
+                      CONCEPT
+                      <span className="text-wayberBlue">.</span> SOLUTION
+                      <span className="text-wayberBlue">.</span> EXECUTE
+                      <span className="text-wayberBlue">.</span>
                     </span>
                   </span>
                 </motion.h1>
