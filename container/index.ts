0e1c46bd626a1fe30660fb97a70d50e62d5d91c3
Vinyl-Davyl <okononfuadavid@gmail.com>
2025-09-30 12:34:56 +0100

    update: development

--- container/index.ts
+++ container/index.ts
@@ -4,7 +4,7 @@ export { default as About } from "./home-page/About";
 export { default as PlayVideo } from "./home-page/Video";
 export { default as Projects } from "./home-page/Projects";
 export { default as Clients } from "./home-page/Clients";
-export { default as VideoHome } from './home-page/Video';
+export { default as VideoHome } from "./home-page/Video";
 
 // services page
 export { default as Heroservices } from "./services-page/Hero";
@@ -15,32 +15,32 @@ export { default as Expectations } from "./services-page/Expectations";
 export { default as Archive } from "./services-page/Archive";
 
 // presentation page
-export { default as Heropresentation } from './presentation-page/Hero';
-export { default as Projectspresentation } from './presentation-page/Projects';
-export { default as Publication } from './presentation-page/Publication';
+export { default as Heropresentation } from "./presentation-page/Hero";
+export { default as Projectspresentation } from "./presentation-page/Projects";
+export { default as Publication } from "./presentation-page/Publication";
 
 // aboute page
-export { default as Heroabout } from './about-page/Hero';
-export { default as Aboutabout } from './about-page/About';
-export { default as Team } from './about-page/Team';
-export { default as Principles } from './about-page/Principles';
-export { default as Partners } from './about-page/Partners';
-export { default as Insights } from './about-page/Insights';
+export { default as Heroabout } from "./about-page/Hero";
+export { default as Aboutabout } from "./about-page/About";
+export { default as Team } from "./about-page/Team";
+export { default as Principles } from "./about-page/Principles";
+export { default as Partners } from "./about-page/Partners";
+export { default as Insights } from "./about-page/Insights";
 
 // insights page
-export { default as Heroinsights } from './insights-page/Hero';
-export { default as Publicationinsights } from './insights-page/Publication';
+export { default as Heroinsights } from "./insights-page/Hero";
+export { default as Publicationinsights } from "./insights-page/Publication";
 
 // contact page
-export { default as Herocontact } from './contact-page/Hero';
-export { default as Form } from './contact-page/Form';
-export { default as FAQ } from './contact-page/Faq';
-export { default as Socials } from './contact-page/Socials';
+export { default as Herocontact } from "./contact-page/Hero";
+export { default as Form } from "./contact-page/Form";
+export { default as FAQ } from "./contact-page/Faq";
+export { default as Socials } from "./contact-page/Socials";
 // workiz page
-export { default as Heroworkiz } from './workiz-page/Hero';
-export { default as Aboutworkiz } from './workiz-page/About';
-export { default as Works } from './workiz-page/Works';
-export { default as Chelenge } from './workiz-page/Chelenge';
-export { default as Result } from './workiz-page/Result';
-export { default as Credit } from './workiz-page/Credit';
-export { default as VideoWorkiz } from './workiz-page/Video';
+export { default as Heroworkiz } from "./workiz-page/Hero";
+export { default as Aboutworkiz } from "./workiz-page/About";
+export { default as Works } from "./workiz-page/Works";
+export { default as Chelenge } from "./workiz-page/Chelenge";
+export { default as Result } from "./workiz-page/Result";
+export { default as Credit } from "./workiz-page/Credit";
+export { default as VideoWorkiz } from "./workiz-page/Video";
